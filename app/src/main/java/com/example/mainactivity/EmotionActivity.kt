d74c0db6a2bc78c94639b2feea1a2e7f693b9123
vi17chou <11136002@ntub.edu.tw>
2023-04-16 17:58:36 +0800

    new app 0416

--- app/src/main/java/com/example/mainactivity/EmotionActivity.kt
+++ app/src/main/java/com/example/mainactivity/EmotionActivity.kt
@@ -2,10 +2,15 @@ package com.example.mainactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class EmotionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_emotion)
+        setContentView(R.layout.table)
+        val back = findViewById<Button>(R.id.btn_method)
+        back.setOnClickListener {
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/mainactivity/MethodActivity.kt
+++ app/src/main/java/com/example/mainactivity/MethodActivity.kt
@@ -9,37 +9,47 @@ class MethodActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_method)
-        val game=findViewById<Button>(R.id.game)
-        val breath=findViewById<Button>(R.id.breath)
-        val sport=findViewById<Button>(R.id.sport)
-        val emotion=findViewById<Button>(R.id.emotion)
-        val diet=findViewById<Button>(R.id.diet)
+        val game = findViewById<Button>(R.id.game)
+        val breath = findViewById<Button>(R.id.breath)
+        val sport = findViewById<Button>(R.id.sport)
+        val emotion = findViewById<Button>(R.id.emotion)
+        val diet = findViewById<Button>(R.id.diet)
+        val back = findViewById<Button>(R.id.back)
 
-
+        //小遊戲
         game.setOnClickListener {
-            val it= Intent(this,GameActivity::class.java)
+            val it = Intent(this, GameActivity::class.java)
             startActivity(it)
         }
 
+        //深呼吸
         breath.setOnClickListener {
-            val it= Intent(this,BreathActivity::class.java)
+            val it = Intent(this, BreathActivity::class.java)
             startActivity(it)
         }
 
+        //運動
         sport.setOnClickListener {
-            val it= Intent(this,SportActivity::class.java)
+            val it = Intent(this, SportActivity::class.java)
             startActivity(it)
         }
 
+        //控制情緒
         emotion.setOnClickListener {
-            val it= Intent(this,EmotionActivity::class.java)
+            val it = Intent(this, EmotionActivity::class.java)
             startActivity(it)
         }
-
+        //控制情緒
         diet.setOnClickListener {
-            val it= Intent(this,DietActivity::class.java)
+            val it = Intent(this, DietActivity::class.java)
             startActivity(it)
         }
 
+        //返回
+        back.setOnClickListener { v ->
+            val it = Intent(this, HomeActivity::class.java).apply {
+
+            }
+        }
     }
 }
