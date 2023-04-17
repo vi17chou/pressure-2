a1693926dc20df9f8256a748bc84a04d94868ebc
vi17chou <120084375+vi17chou@users.noreply.github.com>
2023-04-17 22:46:34 +0800

    New app 0417

--- app/src/main/java/com/example/mainactivity/Sign2Activity.kt
+++ app/src/main/java/com/example/mainactivity/Sign2Activity.kt
@@ -1,30 +1,51 @@
 package com.example.mainactivity
 
+import android.app.DatePickerDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.RadioButton
 import android.widget.RadioGroup
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
+import java.util.Calendar
 
 class Sign2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign2)
         val btn_submit = findViewById<Button>(R.id.submit)
-        val sexy = findViewById<RadioGroup>(R.id.sexy)
-        val dialogBuilder = AlertDialog.Builder(this@Sign2Activity)
+        val sex = findViewById<RadioGroup>(R.id.sexy)
+        val btn_date=findViewById<Button>(R.id.btn_date)
+        val age=findViewById<TextView>(R.id.age)
+        val name=findViewById<TextView>(R.id.name)
+
+        btn_date.setOnClickListener {
+            val c = Calendar.getInstance()
+            val year = c.get(Calendar.YEAR)
+            val month = c.get(Calendar.MONTH)
+            val day = c.get(Calendar.DAY_OF_MONTH)
+            DatePickerDialog(this, { _, year, month, day ->
+                run {
+                    val format = "${setDateFormat(year, month, day)}"
+                    age.text = format
+                }
+            }, year, month, day).show()
+        }
 
         btn_submit.setOnClickListener {
-            AlertDialog.Builder(this)
-                .setPositiveButton("OK"){ dialog, which->
-
-                }
             val b = Bundle()
-            b.putString("sexy", sexy.findViewById<RadioButton>
-                    (sexy.checkedRadioButtonId).text.toString()
-            )
+            b.putString("sex",sex.findViewById<RadioButton>
+                (sex.checkedRadioButtonId).text.toString())
+            b.putString("na",name.getText().toString())
+            b.putString("ag",age.getText().toString())
+            setResult(RESULT_OK, Intent().putExtras(b))
+            finish()
         }
+    }
+    private fun setDateFormat(year: Int, month: Int, day: Int): String {
+        return "$year-${month + 1}-$day"
     }
 }
--- app/src/main/java/com/example/mainactivity/SignActivity.kt
+++ app/src/main/java/com/example/mainactivity/SignActivity.kt
@@ -1,17 +1,35 @@
 package com.example.mainactivity
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class SignActivity : AppCompatActivity() {
+    /*override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        data?.extras?.let {
+            if (requestCode==1 && resultCode == Activity.RESULT_OK){
+                findViewById<TextView>(R.id.newtest).text=
+                    "姓名:${it.getString("na")}\n"+
+                            "出生年月:${it.getString("ag")}\n"+
+                            "性別:${it.getString("sex")}\n"
+            }
+        }
+    }*/
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign)
         val next=findViewById<Button>(R.id.sign_next)
+        val btn_submit=findViewById<Button>(R.id.btn_submit)
         next.setOnClickListener {
-            val it=Intent(this,Sign2Activity::class.java)
+            /*val intent =  Intent(this,Sign2Activity::class.java)
+            startActivityForResult(intent,1)*/
+        }
+        btn_submit.setOnClickListener {
+            val it=Intent(this,MainActivity::class.java)
             startActivity(it)
         }
     }
