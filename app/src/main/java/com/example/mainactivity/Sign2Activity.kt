9a9bc2ac502e9c462c39136b7ed10a82e9b84503
vi17chou <11136002@ntub.edu.tw>
2023-04-17 20:03:42 +0800

    New  app 0417

--- app/src/main/java/com/example/mainactivity/Sign2Activity.kt
+++ app/src/main/java/com/example/mainactivity/Sign2Activity.kt
@@ -1,28 +1,30 @@
 package com.example.mainactivity
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
 import android.widget.RadioButton
 import android.widget.RadioGroup
-import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 
 class Sign2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign2)
-        //日期製造器
-        //date_edit.setOnClickListener(listener)
-        val name=findViewById<TextView>(R.id.name)
-        val age=findViewById<TextView>(R.id.age)
-        val sexy=findViewById<RadioGroup>(R.id.sexy)
+        val btn_submit = findViewById<Button>(R.id.submit)
+        val sexy = findViewById<RadioGroup>(R.id.sexy)
+        val dialogBuilder = AlertDialog.Builder(this@Sign2Activity)
 
-        val b = Bundle()
-        b.putString("na",name.getText().toString())
-        b.putString("ag",age.getText().toString())
-        b.putString("sex",sexy.findViewById<RadioButton>
-            (sexy.checkedRadioButtonId).text.toString())
-        setResult(RESULT_OK, Intent().putExtras(b))
-        finish()
+        btn_submit.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setPositiveButton("OK"){ dialog, which->
+
+                }
+            val b = Bundle()
+            b.putString("sexy", sexy.findViewById<RadioButton>
+                    (sexy.checkedRadioButtonId).text.toString()
+            )
+        }
     }
 }
--- app/src/main/java/com/example/mainactivity/SignActivity.kt
+++ app/src/main/java/com/example/mainactivity/SignActivity.kt
@@ -3,28 +3,16 @@ package com.example.mainactivity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.widget.Button
-import androidx.appcompat.app.AlertDialog
 
 class SignActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign)
         val next=findViewById<Button>(R.id.sign_next)
-        val dialogBuilder = AlertDialog.Builder(this@SignActivity)
-        val submit=findViewById<Button>(R.id.sign_submit)
         next.setOnClickListener {
-            val dialogItem = LayoutInflater.from(this).inflate(R.layout.activity_sign2, null)
-            AlertDialog.Builder(this)
-                .setView(R.layout.activity_sign2)
-                .setPositiveButton("OK"){ dialog, which->
-
-                }.show()
-        }
-
-        submit.setOnClickListener {
-            finish()
+            val it=Intent(this,Sign2Activity::class.java)
+            startActivity(it)
         }
     }
 }
