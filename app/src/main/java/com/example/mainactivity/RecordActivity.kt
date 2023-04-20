9da99e853fb6b1c599454829b5f8892bb8ac69e2
vi17chou <11136002@ntub.edu.tw>
2023-04-20 13:03:53 +0800

    New app 0420-1

--- app/src/main/java/com/example/mainactivity/RecordActivity.kt
+++ app/src/main/java/com/example/mainactivity/RecordActivity.kt
@@ -2,10 +2,28 @@ package com.example.mainactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import java.util.*
 
 class RecordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_record)
+        val btn_rage = findViewById<Button>(R.id.btn_range)
+
+        btn_rage.setOnClickListener {
+            showDatePickerDialog()
+        }
+    }
+
+    private fun showDatePickerDialog() {
+        val now = Calendar.getInstance()
+        /*val dpd = DatePickerDialog.newInstance(
+            this@SelectActivity,
+            now.get(Calendar.YEAR),
+            now.get(Calendar.MONTH),
+            now.get(Calendar.DAY_OF_MONTH)
+        )
+        dpd.show(fragmentManager, "Datepickerdialog")*/
     }
 }
--- app/src/main/java/com/example/mainactivity/SelectActivity.kt
+++ app/src/main/java/com/example/mainactivity/SelectActivity.kt
@@ -1,11 +1,32 @@
 package com.example.mainactivity
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import java.util.Calendar
 
 class SelectActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select)
+        val btn_rage = findViewById<Button>(R.id.btn_range)
+
+        btn_rage.setOnClickListener {
+            showDatePickerDialog()
+        }
     }
+
+    private fun showDatePickerDialog() {
+        val now = Calendar.getInstance()
+        /*val dpd = DatePickerDialog.newInstance(
+            this@SelectActivity,
+            now.get(Calendar.YEAR),
+            now.get(Calendar.MONTH),
+            now.get(Calendar.DAY_OF_MONTH)
+        )
+        dpd.show(fragmentManager, "Datepickerdialog")*/
+    }
+
 }
