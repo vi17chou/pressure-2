13fac71a0a7830d9fbbf398a03358178c187360e
Lai-tw <120080188+Lai-tw@users.noreply.github.com>
2023-04-21 15:51:46 +0800

    NEW0421-1

--- app/src/main/java/com/example/mainactivity/BalloonActivity.kt
+++ app/src/main/java/com/example/mainactivity/BalloonActivity.kt
@@ -2,12 +2,19 @@ package com.example.mainactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class BalloonActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_balloon)
-        //123
-        //是正常的嗎?程式到底怎麼了
+
+        val back5 = findViewById<Button>(R.id.back5)
+
+        back5.setOnClickListener {
+            finish()
+
+        }
+
     }
 }
--- app/src/main/java/com/example/mainactivity/PuzzleActivity.kt
+++ app/src/main/java/com/example/mainactivity/PuzzleActivity.kt
@@ -2,10 +2,19 @@ package com.example.mainactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class PuzzleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_puzzle)
+
+        val back4 = findViewById<Button>(R.id.back4)
+
+        back4.setOnClickListener {
+            finish()
+
+        }
+
     }
 }
