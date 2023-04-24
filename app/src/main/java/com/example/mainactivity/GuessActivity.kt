b4726a97921709e718371200ce65b324af34148c
vi17chou <120084375+vi17chou@users.noreply.github.com>
2023-04-24 21:15:16 +0800

    New app 0424

--- app/src/main/java/com/example/mainactivity/GuessActivity.kt
+++ app/src/main/java/com/example/mainactivity/GuessActivity.kt
@@ -3,13 +3,15 @@ package com.example.mainactivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class GuessActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_guess)
-
+        val guess_submit=findViewById<Button>(R.id.guess_submit)
         val back6 = findViewById<Button>(R.id.back6)
+        val number=findViewById<TextView>(R.id.number)
 
         back6.setOnClickListener {
             finish()
