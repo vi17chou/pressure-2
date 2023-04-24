f526952c03d383eaedbd911116b9b362886c2d06
vi17chou <120084375+vi17chou@users.noreply.github.com>
2023-04-24 22:12:51 +0800

    New app 0424-1

--- app/src/main/java/com/example/mainactivity/GuessActivity.kt
+++ app/src/main/java/com/example/mainactivity/GuessActivity.kt
@@ -1,9 +1,10 @@
 package com.example.mainactivity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 
 class GuessActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +13,19 @@ class GuessActivity : AppCompatActivity() {
         val guess_submit=findViewById<Button>(R.id.guess_submit)
         val back6 = findViewById<Button>(R.id.back6)
         val number=findViewById<TextView>(R.id.number)
+        val historyInput=findViewById<TextView>(R.id.history_input)
+        val historyResult=findViewById<TextView>(R.id.history_result)
+        val game = Game()
+        game.generateAnswer();
+
+        guess_submit.setOnClickListener {
+            historyInput.setText(number.getText().toString() + "\n" + historyInput.getText());
+            historyResult.setText((game.checkAnswer(number.getText().toString()))+"\n"+ historyResult.getText())
+            if(game.isWin){
+                
+            }
+        }
+
 
         back6.setOnClickListener {
             finish()
