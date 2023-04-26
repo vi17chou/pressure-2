4572ffefae7fd38d37504ec152bf1d7e0971a52f
vi17chou <120084375+vi17chou@users.noreply.github.com>
2023-04-26 22:47:51 +0800

    0426-3

--- app/src/main/java/com/example/mainactivity/TestActivity.kt
+++ app/src/main/java/com/example/mainactivity/TestActivity.kt
@@ -4,7 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.TextView
+import java.text.NumberFormat
 
 class TestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,9 +17,21 @@ class TestActivity : AppCompatActivity() {
         //題目
         val content1 = findViewById<TextView>(R.id.content1)
         val textViewString = content1.text.toString()
+        val t0=findViewById<RadioGroup>(R.id.t0)
         content1.text = " 睡眠困難，譬如難以入睡、易醒或早醒"
 
+
         Next.setOnClickListener {
+            val selectedId = t0.checkedRadioButtonId
+            val tipPercentage = when (selectedId) {
+                R.id.one ->0
+                else -> "未選擇"
+
+            }
+            val test=findViewById<TextView>(R.id.textView12)
+            var tip = tipPercentage
+            val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+            test.text = getString(R.string.tip_amount, formattedTip)
             val it = Intent(this, TestActivity2::class.java)
             startActivity(it)
         }
--- app/src/main/java/com/example/mainactivity/TestActivity2.kt
+++ app/src/main/java/com/example/mainactivity/TestActivity2.kt
@@ -6,7 +6,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.RadioGroup
 import android.widget.TextView
+import java.text.NumberFormat
 
 class TestActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,9 +25,20 @@ class TestActivity2 : AppCompatActivity() {
         val content1 = findViewById<TextView>(R.id.content1)
         val textViewString = content1.text.toString()
         content1.text = "感覺緊張不安";
-
+        val t1=findViewById<RadioGroup>(R.id.t1)
         
         Next.setOnClickListener {
+            val selectedId = t1.checkedRadioButtonId
+
+            val tipPercentage = when (selectedId) {
+                R.id.one -> 0
+                else -> "未選擇"
+
+            }
+            val test=findViewById<TextView>(R.id.textView10)
+            var tip2 = tipPercentage
+            val formattedTip = NumberFormat.getCurrencyInstance().format(tip2)
+            test.text = getString(R.string.tip_amount, formattedTip)
             val it = Intent(this,TestActivity3::class.java)
             startActivity(it)
         }
