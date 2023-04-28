3e20f13008a6d3780e050ef6167b6ec4d5b05647
vi17chou <120084375+vi17chou@users.noreply.github.com>
2023-04-28 22:29:45 +0800

    New app 0428-2

--- app/src/main/java/com/example/mainactivity/SQLiteHelper.kt
+++ app/src/main/java/com/example/mainactivity/SQLiteHelper.kt
@@ -1,5 +1,6 @@
 package com.example.mainactivity
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
@@ -16,28 +17,43 @@ object FeedReaderContract {
  class SQLiteHelper{
     class FeedReaderDbHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
         override fun onCreate(db: SQLiteDatabase) {
+
+            //使用者紀錄
             val USERS =
                 "CREATE TABLE IF NOT EXISTS USERS(" +
-                        "id INTEGER PRIMARY KEY AUTOINCREMENT  ," +
+                        "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                        "User_id INTEGER (10),"+
                         "NAME varchar(10),"+
                         "age date,"+
-                        "MAX_DATE varchar(50)" +
+                        "SEXY varchar(1),"+
+                        "ACCOUN varchar(10),"+
+                        "PASSWORD varchar(10),"+
+                        "CHECKPWD varchar(10)" +
                 ");";
 
+            /*測驗紀錄
             val TEST =
                 "CREATE TABLE IF NOT EXISTS USERS(" +
-                        "id INTEGER PRIMARY KEY AUTOINCREMENT  ," +
-                        "id INTEGER PRIMARY KEY AUTOINCREMENT  , "+
-                        "id INTEGER PRIMARY KEY AUTOINCREMENT  ," +
-                        "MAX_DATE varchar(50)" +
+                        "User_id INTEGER (10) PRIMARY KEY ," +
+                        "T1 INTEGER(1),"+
+                        "T2 INTEGER(1),"+
+                        "T3 INTEGER(1),"+
+                        "T4 INTEGER(1),"+
+                        "T5 INTEGER(1),"+
+                        "T6 INTEGER(1),"+
+                        "T7 INTEGER(1),"+
+                        "T8 INTEGER(1),"+
+                        "T9 INTEGER(1),"+
+                        "T10 INTEGER(1),"+
+                        "T11 INTEGER(1),"+
+                        "TEST_DATE date" +
                         ");";
 
             val DIARY =
                 "CREATE TABLE IF NOT EXISTS USERS(" +
-                        "id INTEGER PRIMARY KEY AUTOINCREMENT  ," +
-                        "id INTEGER PRIMARY KEY AUTOINCREMENT  , "+
-                        "id INTEGER PRIMARY KEY AUTOINCREMENT  ," +
-                        "MAX_DATE varchar(50)" +
+                        "User_id INTEGER (10) PRIMARY KEY ," +
+                        "Diary varchar(500), "+
+                        "Diary_Date date" +
                         ");";
 
             val SPORT =
@@ -48,14 +64,15 @@ object FeedReaderContract {
                         "MAX_DATE varchar(50)" +
                         ");";
 
+             */
             db.execSQL(USERS)
-            db.execSQL(TEST)
-            db.execSQL(DIARY)
-            db.execSQL(SPORT)
+
+            //db.execSQL(TEST)
+            //db.execSQL(DIARY)
+            //db.execSQL(SPORT)
         }
         override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
-            // This database is only a cache for online data, so its upgrade policy is
-            // to simply to discard the data and start over
+
             //db.execSQL(SQL_DELETE_ENTRIES)
             onCreate(db)
         }
@@ -66,6 +83,10 @@ object FeedReaderContract {
             // If you change the database schema, you must increment the database version.
             const val DATABASE_VERSION = 1
             const val DATABASE_NAME = "FeedReader.db"
+
         }
+        val dbHelper = FeedReaderDbHelper(context)
+        val db = dbHelper.writableDatabase
+
     }
 }
