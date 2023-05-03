cdfff4fa352fcc013dabbcde6e7febb81d2532ba
Lai-tw <120080188+Lai-tw@users.noreply.github.com>
2023-05-03 23:15:59 +0800

    0503-3

--- app/src/main/java/com/example/mainactivity/DietActivity.kt
+++ app/src/main/java/com/example/mainactivity/DietActivity.kt
@@ -1,10 +1,13 @@
 package com.example.mainactivity
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 
 class DietActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.diet_table)
