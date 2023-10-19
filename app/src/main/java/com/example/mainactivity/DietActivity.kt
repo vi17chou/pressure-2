19bfdff4c3cacbab091921b05ae0b48e6dd69ae0
Lai-tw <120080188+Lai-tw@users.noreply.github.com>
2023-10-19 23:22:54 +0800

    Revert "1019-3"
    This reverts commit eb54be73d7db37281e18a4d4fcfc6c78f0a08471.

--- app/src/main/java/com/example/mainactivity/DietActivity.kt
+++ app/src/main/java/com/example/mainactivity/DietActivity.kt
@@ -13,52 +13,46 @@ class DietActivity : AppCompatActivity() {
         setContentView(R.layout.activity_diet)//現在的layout是顯示原始的畫面
         //設定隱藏標題
         getSupportActionBar()?.hide()
-        val img_diet = findViewById<ImageButton>(R.id.img_diet)
+        val img_diet=findViewById<ImageButton>(R.id.img_diet)
         img_diet.setOnClickListener {
-            val it = Intent(this, Diet_food1Activity::class.java)
+            val it= Intent(this,Diet_food1Activity::class.java)
             startActivity(it)
         }
 
-        val tea = findViewById<ImageButton>(R.id.tea)
+        val tea=findViewById<ImageButton>(R.id.tea)
         tea.setOnClickListener {
-            val it = Intent(this, Diet_food2Activity::class.java)
+            val it= Intent(this,Diet_food2Activity::class.java)
             startActivity(it)
         }
 
-        val banana = findViewById<ImageButton>(R.id.banana)
+        val banana=findViewById<ImageButton>(R.id.banana)
         banana.setOnClickListener {
-            val it = Intent(this, Diet_food3Activity::class.java)
+            val it= Intent(this,Diet_food3Activity::class.java)
             startActivity(it)
         }
 
-        val oat = findViewById<ImageButton>(R.id.oat)
+        val oat=findViewById<ImageButton>(R.id.oat)
         oat.setOnClickListener {
-            val it = Intent(this, Diet_food4Activity::class.java)
+            val it= Intent(this,Diet_food4Activity::class.java)
             startActivity(it)
         }
 
-        val cheese = findViewById<ImageButton>(R.id.cheese)
+        val cheese=findViewById<ImageButton>(R.id.cheese)
         cheese.setOnClickListener {
-            val it = Intent(this, Diet_food5Activity::class.java)
+            val it= Intent(this,Diet_food5Activity::class.java)
             startActivity(it)
         }
 
-        val og = findViewById<ImageButton>(R.id.og)
+        val og=findViewById<ImageButton>(R.id.og)
         og.setOnClickListener {
-            val it = Intent(this, Diet_food6Activity::class.java)
+            val it= Intent(this,Diet_food6Activity::class.java)
             startActivity(it)
         }
 
-        val chocolate = findViewById<ImageButton>(R.id.chocolate)
+        val chocolate=findViewById<ImageButton>(R.id.chocolate)
         chocolate.setOnClickListener {
-            val it = Intent(this, Diet_food7Activity::class.java)
+            val it= Intent(this,Diet_food7Activity::class.java)
             startActivity(it)
-        }
-
-        val back3 = findViewById<ImageButton>(R.id.back3)
-
-        back3.setOnClickListener {
-            finish()
         }
     }
 }
