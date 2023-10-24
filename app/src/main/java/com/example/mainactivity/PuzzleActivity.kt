e8f1898b064369a6d15b1cdf3714267e2ae40e7d
Lai-tw <120080188+Lai-tw@users.noreply.github.com>
2023-10-24 10:47:59 +0800

    1024

--- app/src/main/java/com/example/mainactivity/PuzzleActivity.kt
+++ app/src/main/java/com/example/mainactivity/PuzzleActivity.kt
@@ -41,7 +41,7 @@ class PuzzleActivity : AppCompatActivity() {
     private val handler: Handler = object : Handler() {
         override fun handleMessage(msg: Message) {
             if (msg.what == 1) {
-                textView.text = "时间：$time"
+                textView.text = "時間：$time"
                 if (timeswitch) {
                     time++
                     sendEmptyMessageDelayed(1, 1000)
@@ -51,11 +51,48 @@ class PuzzleActivity : AppCompatActivity() {
     }
 
 
+    private fun showGameInstructions() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("遊戲說明")
+        builder.setMessage("在遊戲中，您需要點擊拼圖，並與空白區塊交换位置將他們按照正确的顺序排列，直到拼圖成功!")
+        builder.setPositiveButton("確認") { dialog, which ->
+            // 用户點擊確認按鈕後開始計時遊戲
+            startGame()
+        }
+        builder.show()
+    }
+
+
+   //開始遊戲計時
+    private fun startGame() {
+        time = 0
+        restore()
+        textView.text = "時間：$time"
+        timeswitch = true
+       //每隔 1 秒更新一次遊戲界面中的時間顯示
+        handler.sendEmptyMessageDelayed(1, 1000)
+        button01.isClickable = true
+        button02.isClickable = true
+        button03.isClickable = true
+        button04.isClickable = true
+        button05.isClickable = true
+        button06.isClickable = true
+        button07.isClickable = true
+        button08.isClickable = true
+        button09.isClickable = true
+        random()
+    }
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_puzzle)
         //設定隱藏標題
         getSupportActionBar()?.hide();
+
+        // 顯示遊戲說明AlertDialog
+        showGameInstructions()
 
         button01 = findViewById(R.id.pt_ib_00x01)
         button02 = findViewById(R.id.pt_ib_00x02)
@@ -68,6 +105,7 @@ class PuzzleActivity : AppCompatActivity() {
         button09 = findViewById(R.id.pt_ib_00x09)
         textView = findViewById(R.id.pt_tv_time)
         buttonrestart = findViewById(R.id.pt_btn_restart)
+        //每隔 1 秒更新一次遊戲界面中的時間顯示
         handler.sendEmptyMessageDelayed(1, 1000)
         random()
 
@@ -80,21 +118,28 @@ class PuzzleActivity : AppCompatActivity() {
 
     }
 
+    //隨機打亂拼圖
     private fun random() {
+        //計時器是否開始計時
         timeswitch = true
         for (i in imageIndex.indices) {
             imageIndex[i] = i
         }
+        //用於儲存随機生成的值
         var rand1: Int
         var rand2: Int
+        //随機打亂拼圖排列的次数
         for (j in 0 until 20) {
+          //  生成一個随機變數 rand1，範圍 0 到 length - 1 之間，其中 length 表示拼圖數量。
             rand1 = (Math.random() * (length - 1)).toInt()
+            //生成一個随機變數 rand2，確保與 rand1 不相同
             do {
                 rand2 = (Math.random() * (length - 1)).toInt()
                 if (rand1 != rand2) {
                     break
                 }
             } while (true)
+            //用 swap 函数，交换 rand1 和 rand2 位置的拼圖，打亂它們的排列。
             swap(rand1, rand2)
         }
         button01.setImageDrawable(resources.getDrawable(image[imageIndex[0]]))
@@ -108,12 +153,16 @@ class PuzzleActivity : AppCompatActivity() {
         button09.setImageDrawable(resources.getDrawable(image[imageIndex[8]]))
     }
 
+   //交換拼圖位置
+    //用于交換兩個拼圖在 imageIndex 中的位置。有兩個参数 rand1 和 rand2，分别表示要交換的兩個拼圖在 imageIndex 数组中的索引
     private fun swap(rand1: Int, rand2: Int) {
+      // 用於存 imageIndex 中 為rand1 的元素的值。
         val temp = imageIndex[rand1]
         imageIndex[rand1] = imageIndex[rand2]
         imageIndex[rand2] = temp
     }
 
+//點擊拼圖
     fun onClick(view: View) {
         val id = view.id
         when (id) {
@@ -129,45 +178,67 @@ class PuzzleActivity : AppCompatActivity() {
         }
     }
 
+    //移動拼圖
     private fun move(imagbtnId: Int, site: Int) {
+        //用於計算被點擊的拼圖的位置坐标，sitex 表示横向坐標，sitey 表示縱向坐標
         val sitex = site / imagex
         val sitey = site % imagey
+        //用於獲取空白塊的位置坐標
         val blankx = blankSwap / imagex
         val blanky = blankSwap % imagey
+        //計算被點擊的拼圖與空白區域之間的横向和縱向距離差，分别存在 x 和 y ，這些差值用來判断是否可以移動拼圖
         val x = Math.abs(sitex - blankx)
         val y = Math.abs(sitey - blanky)
+        //用於檢查被點擊的拼圖是否可以與空白區域交換位置。如果 x 和 y 的差值中有一个為 1（表示相鄰的位置），則允許移動
         if ((x == 0 && y == 1) || (x == 1 && y == 0)) {
+            //用于查找並獲取被點擊的拼圖(ImageButton )，並存在 clickButton
             val clickButton = findViewById<ImageButton>(imagbtnId)
+            //將被點擊的拼圖設置為不可見，表示拼圖被移走。
             clickButton.visibility = View.INVISIBLE
+            //用於查找並獲取目前的空白區域(ImageButton)並存在 blankButton
             val blankButton = findViewById<ImageButton>(blankImgid)
+            //將空白區域的拼圖設置為被點擊的拼圖。这是拼圖交换位置的效果。
             blankButton.setImageDrawable(resources.getDrawable(image[imageIndex[site]]))
+            //將空白區域設置為可見，表示空白區域移動到了被點擊的拼圖的位置。
             blankButton.visibility = View.VISIBLE
+           // 用 swap 方法，用來交换 imageIndex 数组中的值，以實際執行拼圖的移動。
             swap(site, blankSwap)
+            //更新 blankSwap 的值，以反映空白區域的新位置。
             blankSwap = site
+           // 更新 blankImgid ，以反映空白區域所在的 ImageButton  id。
             blankImgid = imagbtnId
         }
+        //用於檢查遊戲是否已结束。
         gameOver()
     }
 
+//還原遊戲狀態
     private fun restore() {
+        // 用於停止或取消計時
         handler.removeMessages(1)
         val clickButton = findViewById<ImageButton>(blankImgid)
+        //初始空白區域（clickButton）設置為可見
         clickButton.visibility = View.VISIBLE
         val blankButton = findViewById<ImageButton>(R.id.pt_ib_00x09)
+        //初始空白區域（blankButton）設置為不可見
         blankButton.visibility = View.INVISIBLE
+        //將 blankImgid 更新為初始空白區域的 id，以确保在下一次移動時，正确的空白區域將被操作
         blankImgid = R.id.pt_ib_00x09
         blankSwap = length - 1
     }
 
+    //判斷遊戲是否結束
     private fun gameOver() {
-        var loop = true
+        var finish = true
+        //在for迴圈中，檢查每個拼圖(imageIndex[i])是否在正確的位置，如果是遊戲就結束，timeswitch就會停止計時
         for (i in imageIndex.indices) {
             if (imageIndex[i] != i) {
-                loop = false
+                finish = false
             }
         }
-        if (loop) {
+        if (finish) {
             timeswitch = false
+            //是否可點擊(isClickable)
             button01.isClickable = false
             button02.isClickable = false
             button03.isClickable = false
@@ -178,22 +249,26 @@ class PuzzleActivity : AppCompatActivity() {
             button08.isClickable = false
             button09.isClickable = false
             button09.setImageDrawable(resources.getDrawable(image[8]))
-            button09.visibility = View.VISIBLE
+            button09.visibility = View.VISIBLE//用於是否可見
+           // 用於停止或取消計時
             handler.removeMessages(1)
             val builder = AlertDialog.Builder(this@PuzzleActivity)
-            builder.setMessage("恭喜，拼图成功！您用的时间为$time 秒")
-                .setPositiveButton("确认", null)
+            builder.setMessage("恭喜，拼圖成功！您用的時間為$time 秒")
+                .setPositiveButton("確認", null)
             val dialog = builder.create()
             dialog.show()
         }
     }
 
+    //重新開始遊戲
     fun restart(view: View) {
         time = 0
         restore()
-        textView.text = "时间：$time"
+        textView.text = "時間：$time"
         timeswitch = true
+        //每隔 1 秒更新一次遊戲界面中的時間顯示
         handler.sendEmptyMessageDelayed(1, 1000)
+        //是否可點擊(isClickable)
         button01.isClickable = true
         button02.isClickable = true
         button03.isClickable = true
