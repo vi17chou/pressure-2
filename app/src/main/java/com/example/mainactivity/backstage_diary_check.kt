f7ff00d9bd30d62785c48e0c73a3f77493700296
chaiyi027 <11136027@ntub.edu.tw>
2023-10-27 22:45:52 +0800

    new backstage_diary

---
+++ app/src/main/java/com/example/mainactivity/backstage_diary_check.kt
@@ -0,0 +1,11 @@
+package com.example.mainactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class backstage_diary_check : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_backstage_diary_check)
+    }
+}
